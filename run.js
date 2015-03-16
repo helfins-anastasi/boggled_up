2041d202cee6ec1c23153d591df98d4197a182b5
Zachary Helfinstein <zhelfinstein@csil.cs.ucsb.edu>
2015-03-15 23:29:06 -0700

    Modified code structure to use more OOP

--- run.js
+++ run.js
@@ -10,39 +10,44 @@ for(var letter in frequencies){
 //Set up colors
 RED = "rgb(230,0,0)", GREEN = "rgb(0,180,0)", WHITE = "white";
 
+//Create a board space
+function Space(color) {
+	this.letter = letters[Math.floor(Math.random()*letters.length)];
+	this.color = color;
+}
+
 //Set up current board
-currentBoard = {};
-currentBoardWidth = 10;
-currentBoardLength = 13;
-currentPlayer = GREEN;
-for(var i = 0; i < currentBoardWidth; ++i) {
-	currentBoard[i] = {};
-	for(var j = 0; j < currentBoardLength; ++j) {
-		currentBoard[i][j] = {letter:letters[Math.floor(Math.random()*letters.length)], 
-					color:(j===0 ? GREEN : (j===12 ? RED : WHITE))};
+function Board(width, length, player) {
+	this.width = width;
+	this.length = length;
+	this.currentPlayer = player;
+	for(var i = 0; i < width; ++i) {
+		this[i] = [];
+	for(var j = 0; j < length; ++j) {
+		currentboard[i][j] = new Space(j===0 ? GREEN : (j===12 ? RED : WHITE));
 	}
 }
 
 //Place for setup that has to be done after page loads
 function go() {
 	console.log("Code is running");
-	drawBoard();
+	drawBoard(new Board(10,13,GREEN));
 	$("body").append('<button id="changePlayer" onclick="changePlayer();">change player</button>');
 }
 
 //Draws the board
-function drawBoard() {
+function drawBoard(currentBoard) {
 	var size = 30;
-	var str = '<svg id="svg" width="'+10*size+'" height="'+13*size+'">';
-	for(var i = 0; i < 10; i++) {
-		for(var j = 0; j < 13; j++) {
+	var str = '<svg id="svg" width="'+currentBoard.width*size+'" height="'+currentBoard.length*size+'">';
+	for(var i = 0; i < currentBoard.width; i++) {
+		for(var j = 0; j < currentBoard.length; j++) {
 			str+='<g id="'+makeId(i,j)+'">';
 			str+='<rect x="'+size*i+'" y="'+size*j;
 			str+='" width ="'+size+'" height="'+size;
 			str+='" style="fill:'+currentBoard[i][j].color;
 			str+=';stroke:black;" ';
-			str+='onclick="selectBoggleSquare('+i+','+j+');"></rect>';			     
-			str+='<text x="'+Math.floor(size*(i+0.2))+'" y="'+ Math.floor(size*(j+0.8))+'" font-size="20px">'+currentBoard[i][j].letter+'</text></g>'
+			str+='onclick="selectBoggleSquare('+i+','+j+');"></rect>';
+			str+='<text x="'+Math.floor(size*(i+0.2))+'" y="'+ Math.floor(size*(j+0.8))+'" font-size="20px" onclick="selectBoggleSquare('+i+','+j+');">'+currentBoard[i][j].letter+'</text></g>'
 		str+="</g>";
 		}
 	}
@@ -53,6 +58,15 @@ function drawBoard() {
 //Abstracts the id for each square to just its index
 function makeId(i,j) {
 	return "space"+i+"_"+j;
+}
+
+function eraseDetachedPortion(i,j) {
+	console.log("erasing segment");
+	//Determine who the other player is
+	var otherPlayer = currentPlayer === GREEN ? RED : GREEN;
+	var condition = function(x,y){ return currentBoard[x][y].color === otherPlayer; };
+	var action = function(x,y){ if(!isConnected(x,y,otherPlayer)) eraseThread(x,y,otherPlayer); };
+	examineNeighbors(i,j,condition,action);
 }
 
 //Called when a square is selected
@@ -75,13 +89,6 @@ function changePlayer() {
 	}
 }
 
-function erasedDetachedPortion(i,j) {
-	var otherPlayer = currentPlayer === GREEN ? RED : GREEN;
-	var condition = function(x,y){ return currentBoard[x][y].color === otherPlayer; };
-	var action = function(x,y){ if(!isConnected(x,y,otherPlayer)) eraseThread(x,y,otherPlayer); };
-	examineNeighbors(i,j,condition,action);
-}
-
 //Examines the neighbors of square (i,j). If conditionFunction(x,y) is true, then actionFunction(x,y)
 function examineNeighbors(i,j,conditionFunction,actionFunction) {
 	for(var x = i-1; x <= i+1; ++x) {
@@ -98,8 +105,34 @@ function examineNeighbors(i,j,conditionFunction,actionFunction) {
 }
 
 function isConnected(i,j,color) {
-	var q = new Queue();
-	
+	var q = [];
+	q.push({i:i,j:j});
+	while(q.length > 0) {
+		for(var x = i-1; x <= i+1; ++x) {
+			if(x < 0 || x >= currentBoardWidth) {
+				continue;
+			} else for(var y = j-1; y <= j+1; ++y) {
+				if(y <= 0 || y >= currentBoardLength || x === y) {
+					continue;
+				}  else if(currentBoard[x][y].color === color) {
+					if((y === 0 && color === GREEN) || (y === currentBoardLength-1 && color === RED)) {
+						return true;
+					}
+					q.push({i:x, j:y});
+				}
+			}
+		}
+	} 
+/*	var condition = function(x,y) { return currentBoard[x][y].color == color; };
+	var action = function(x,y) { }
+	var folder = function(a,b) { return a || b; }
+*/
+}
+
+function eraseThread(i,j,color) {
+	var condition = function(x,y) { return currentBoard[x][y].color === color; };
+	var action = function(x,y) { currentBoard[x][y].color = WHITE; eraseThread(x,y,color); };
+	examineNeighbors(i,j,condition,action);
 }
 
 function Queue() {
@@ -110,7 +143,10 @@ function Queue() {
     this.pop = function() {
         return items.shift();                                                
     }
-    this.peek = function(){
+    this.peek = function() {
         return items[0];                  
+    }
+    this.isEmpty = function() {
+	return items.length == 0;
     }
 }
